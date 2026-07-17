29a8b67c413e067a1f3d09296fd0e376f859c4b9
Aradoxis <99507343+Aradoxis@users.noreply.github.com>
2026-07-17 19:37:38 -0400

    processing time

--- src/player.py
+++ src/player.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+import math, time
 from gameSetting import *
 from gameFunctions import *
 
@@ -20,15 +20,27 @@ def playerAction(playerType, board, pnum, screen=None, event=None):
             return False
 
     elif playerType == "random":
+        start_time = time.perf_counter()
         row, col = randomAgent(board)
+        end_time = time.perf_counter()
+        processing_time = end_time-start_time
+        print(f"Processing time: {processing_time:.6f}")
         drop_piece(board, row, col, pnum, screen)
         
     elif playerType == "ruleBased":
+        start_time = time.perf_counter()
         row, col = ruleBasedAgent(board, pnum)
+        end_time = time.perf_counter()
+        processing_time = end_time-start_time
+        print(f"Processing time: {processing_time:.6f}")
         drop_piece(board, row, col, pnum, screen)
 
     elif playerType == "miniMax":
+        start_time = time.perf_counter()
         best_col, best_score = miniMaxAgent(board, pnum, MINIMAX_DEPTH, -math.inf, math.inf, True)
+        end_time = time.perf_counter()
+        processing_time = end_time-start_time
+        print(f"Processing time: {processing_time:.6f}")
         drop_piece(board, get_next_open_row(board, best_col), best_col, pnum, screen)
     else:
         print("Invalid player type. Please choose 'user', 'random', 'ruleBased', or 'miniMax'.")
